f49180095b775164183b2ac2cd5447c85ac2e3a2
Oguntoyinbo Boluwatife <81614588+Teepheh-Git@users.noreply.github.com>
2021-09-02 13:25:33 +0100

    Amplify Auth

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
-import {FlatList, StyleSheet, View, Text} from 'react-native';
+import {FlatList, StyleSheet, View, Text, Pressable} from 'react-native';
 import ChatRoomItem from "../components/chatRoomItem";
 import chatRoomsData from '../assets/dummy-data/ChatRooms'
+import {Auth} from 'aws-amplify'
 
 export default function HomeScreen() {
 
+
+    function logOut() {
+        Auth.signOut()
+    }
 
     return (
         <View style={styles.page}>
@@ -15,9 +20,17 @@ export default function HomeScreen() {
                 showsVerticalScrollIndicator={false}
                 keyExtractor={item => item.id}
             />
+            <Pressable onPress={logOut} style={{
+                backgroundColor: 'red',
+                height: 50,
+                margin: 10,
+                borderRadius: 50,
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}>
+                <Text>Logout</Text>
+            </Pressable>
 
-
-            {/*<ChatRoomItem chatRoom={chatRoom1} />*/}
         </View>
     );
 };
